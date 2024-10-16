8436247f08bc51dc84c733452fc828a2e9eae5ce
son4ez1 <113089517+son4ez1@users.noreply.github.com>
2024-10-16 13:29:35 +0300

    Add files via upload

---
+++ Полюс/pages/Operator.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (    
+    QDialog # это базовый класс диалогового окна
+)
+
+from PyQt5.uic import loadUi # загрузка интерфейса, созданного в Qt Creator
+
+import sqlite3
+
+class Operator(QDialog):
+    def __init__(self):        
+        super(Operator, self).__init__()
+        print("ok")
